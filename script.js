69d2966e8793d5b6253f2c6bfd1eb728f751ecb6
HailGanyu <henryyanguk@gmail.com>
2022-09-20 22:18:24 +0100

    attempt invert heartShape

--- script.js
+++ script.js
@@ -50,12 +50,19 @@
 
         heartShape.moveTo( 25, 25 );
 
-        heartShape.bezierCurveTo(  25 , 25,20, 0 , 0, 0);
-        heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
-        heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
-        heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
-        heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
-        heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+        //heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+        //heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
+        //heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
+        //heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+        //heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+        //heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+
+        heartShape.bezierCurveTo( 25, 25, 25, 25, 0, 35);
+        heartShape.bezierCurveTo( 0, 50, 0, 80, 35, 80);
+        heartShape.bezierCurveTo( 35, 80, 55, 80, 77, 60);
+        heartShape.bezierCurveTo( 95, 25, 77 , -10, 55, -30);
+        heartShape.bezierCurveTo( 35, -30, 35, -30, 0, -30);
+        heartShape.bezierCurveTo( 0, 0, 0, 20, 25, 25);
         
         const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
