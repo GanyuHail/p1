d1f4ab03c550e8d8b633dac33ccd73b277748327
HailGanyu <henryyanguk@gmail.com>
2022-09-20 15:58:53 +0100

    heart shape edits and raycaster

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    var scene, camera, renderer;
+    var scene, camera, renderer, raycaster;
     var container, HEIGHT,
         WIDTH, fieldOfView, aspectRatio,
         nearPlane, farPlane,
@@ -49,19 +49,35 @@
         const x = 0, y = 0;
         const heartShape = new THREE.Shape();
 
-        heartShape.moveTo(x + 5, y + 5);
-        heartShape.bezierCurveTo(x + 5, y + 5, x + 4, y, x, y);
-        heartShape.bezierCurveTo(x - 6, y, x - 6, y + 7, x - 6, y + 7);
-        heartShape.bezierCurveTo(x - 6, y + 11, x - 3, y + 15.4, x + 5, y + 19);
-        heartShape.bezierCurveTo(x + 12, y + 15.4, x + 16, y + 11, x + 16, y + 7);
-        heartShape.bezierCurveTo(x + 16, y + 7, x + 16, y, x + 10, y);
-        heartShape.bezierCurveTo(x + 7, y, x + 5, y + 5, x + 5, y + 5);
+        heartShape.moveTo( 25, 25 );
+        heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
+        heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
+        heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
+        heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
+        heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
+        heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
+        
+        const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
 
         const heartGeometry = new THREE.ShapeGeometry(heartShape);
         const heartTex = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/p1/main/src/weOpMin.jpg');
         const heartMaterial = new THREE.MeshStandardMaterial({ map: heartTex });
         const heartMesh = new THREE.Mesh(heartGeometry, heartMaterial);
         scene.add(heartMesh);
+
+        const raycaster = new THREE.Raycaster()
+
+        function raycast()
+        {
+          var cameradirection = new THREE.Vector3();
+          camera.getWorldDirection(cameradirection);
+          cameradirection.normalize();
+          var cameraorigin = new THREE.Vector3();
+          camera.getWorldPosition(cameraorigin);
+          var raycaster = new THREE.Raycaster(cameraorigin, cameradirection);
+          var intersects = raycaster.intersectObjects([heartMesh], false);
+          if (intersects.length) {window.open("./store.html");};
+        };
 
         container = document.createElement('div');
         document.body.appendChild(container);
