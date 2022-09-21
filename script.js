26e1f88928cc26a1211f5535fcc30df57250e9b1
HailGanyu <henryyanguk@gmail.com>
2022-09-21 10:24:15 +0100

    box geom for painting

--- script.js
+++ script.js
@@ -46,34 +46,15 @@
         spotLight.position.set(100, 64, 32);
         scene.add(spotLight);
 
-        const heartShape = new THREE.Shape();
+        const paintGeometry = new THREE.BoxGeometry(10, 10, 1);
 
-        heartShape.moveTo( 25, 25 );
-
-        //heartShape.bezierCurveTo( 25, 25, 20, 0, 0, 0 );
-        //heartShape.bezierCurveTo( - 30, 0, - 30, 35, - 30, 35 );
-        //heartShape.bezierCurveTo( - 30, 55, - 10, 77, 25, 95 );
-        //heartShape.bezierCurveTo( 60, 77, 80, 55, 80, 35 );
-        //heartShape.bezierCurveTo( 80, 35, 80, 0, 50, 0 );
-        //heartShape.bezierCurveTo( 35, 0, 25, 25, 25, 25 );
-
-        heartShape.bezierCurveTo( 25, 25, 25, 25, 0, 35);
-        heartShape.bezierCurveTo( 0, 50, 0, 80, 35, 80);
-        heartShape.bezierCurveTo( 35, 80, 55, 80, 77, 60);
-        heartShape.bezierCurveTo( 95, 25, 77 , -10, 55, -30);
-        heartShape.bezierCurveTo( 35, -30, 35, -30, 0, -30);
-        heartShape.bezierCurveTo( 0, 0, 0, 20, 25, 25);
-        
-        const extrudeSettings = { depth: 8, bevelEnabled: true, bevelSegments: 2, steps: 2, bevelSize: 1, bevelThickness: 1 };
-
-        const heartGeometry = new THREE.ExtrudeGeometry(heartShape, extrudeSettings);
-        const heartTex = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/p1/main/src/weOpMin.jpg');
-        const heartMaterial = new THREE.MeshStandardMaterial({ map: heartTex });
-        const heartMesh = new THREE.Mesh(heartGeometry, heartMaterial);
-        scene.add(heartMesh);
+        const paintTex = new THREE.TextureLoader().load('https://raw.githubusercontent.com/GanyuHail/p1/main/src/weOpMin.jpg');
+        const paintMaterial = new THREE.MeshStandardMaterial({ map: paintTex });
+        const paintMesh = new THREE.Mesh(paintGeometry, paintMaterial);
+        scene.add(paintMesh);
 
         const raycaster = new THREE.Raycaster()
-        var intersects = raycaster.intersectObjects([heartMesh], false);
+        var intersects = raycaster.intersectObjects([paintMesh], false);
         if (intersects.length) {window.open("./store.html");};
 
         container = document.createElement('div');
