df96c3412df83defa106426b376773f491c6a7e8
HailGanyu <henryyanguk@gmail.com>
2022-09-22 14:11:57 +0100

    remove require three & camera controls

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { WebGLObjects } = require("three");
-
 (function () {
     'use strict';
     var scene, camera, renderer, raycaster, OrbitControls;
@@ -134,8 +132,7 @@ const { WebGLObjects } = require("three");
 
         camera.position.x += (mouseX - camera.position.x) * 0.1;
         camera.position.y += (-mouseY - camera.position.y) * 0.1;
-        while (camera.position.z < 10) {
-            camera.position.z += (mouseY - camera.position.z) * 0.001};
+        camera.position.z += (mouseY - camera.position.z) * 0.001;
         
         camera.lookAt(scene.position);
 
