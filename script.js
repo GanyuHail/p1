5fbba29af59fab78d1a059901c725d9b61a4adb9
HailGanyu <henryyanguk@gmail.com>
2022-09-22 18:10:48 +0100

    added return to raycaster click

--- script.js
+++ script.js
@@ -158,7 +158,7 @@
             raycaster.setFromCamera(pointer, camera);
             const intersects = raycaster.intersectObjects(scene.children);
             for (let i = 0; i < intersects.length; i++) {
-                if (intersects.length) { window.open("./store.html") };
+                if (intersects.length) { window.open("./store.html"); return; };
             }
         }
 
