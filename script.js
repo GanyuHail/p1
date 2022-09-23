e6455ff686996c5e99d35c73072ce835ac6a1b3a
HailGanyu <henryyanguk@gmail.com>
2022-09-23 16:03:57 +0100

    push raycaster code

--- script.js
+++ script.js
@@ -159,7 +159,7 @@
             raycaster.setFromCamera(pointer, camera);
             const intersects = raycaster.intersectObjects(scene.children);
 
-            if(raycaster.intersects[0] === paintMesh) {
+            if (raycaster.intersects[e] === paintMesh) {
                 window.open("./store.html");
             };
         };
