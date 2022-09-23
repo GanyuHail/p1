0fa717560b4ad0f965b967d6e6d8d2c7f218a87c
HailGanyu <henryyanguk@gmail.com>
2022-09-23 16:35:13 +0100

    raycaster fix 6

--- script.js
+++ script.js
@@ -154,12 +154,15 @@
 
             e.preventDefault();
 
-            const raycaster = new THREE.Raycaster();
-            const pointer = new THREE.Vector2();
-            raycaster.setFromCamera(pointer, camera);
-            const intersects = raycaster.intersectObjects(scene.children);
+            mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+            var ray = new THREE.Raycaster(camera.position, vector.sub( camera.position ).normalize());
+            var vector = new THREE.Vector3 (mouse.x, mouse.y, 1);
+            projector.unprojectVector( vector, camera );
+            //raycaster.setFromCamera(pointer, camera);
+            var intersects = ray.intersectObjects(scene.children);
 
-            if (raycaster.intersects === (scene.children)) {
+            if (intersects.length > 0) {
                 window.open("./store.html");
             };
         };
